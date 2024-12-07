59979fc98bf518d6be44abe8266e13b7ae7cc21a
Karolinova <karolina.nowinska13@gmail.com>
2024-12-07 15:06:46 +0100

    add LogIn method

--- Controller/ListController.cs
+++ Controller/ListController.cs
@@ -1,1 +1,26 @@
 // ListController for methods for application
+public class ListController : Controller
+{
+    public IActionResult LogIn()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult LogIn(Uzytkownik user)
+    {
+
+        var dbContext = new TDLdBContext();
+        var uzytkLog = dbContext.Uzytkownik.Where(a => a.Login == user.Login).Count();
+        var uzytkHas = dbContext.Uzytkownik.Where(a => a.Haslo == user.Haslo).Count();
+        if (uzytkLog > 0 && uzytkHas > 0)
+        {
+            return View();//redirect to main page with list of tasks
+        }
+        else
+        {
+            return View(user);
+        }
+        //return View(user);
+    }
+}
