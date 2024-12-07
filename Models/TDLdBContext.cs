b379b6e5f50b647605da3add00c68d63afa7bf67
Karolinova <karolina.nowinska13@gmail.com>
2024-12-07 14:52:31 +0100

    add model uzytkownik to TDLdBContext

--- Models/TDLdBContext.cs
+++ Models/TDLdBContext.cs
@@ -8,6 +8,7 @@ public class TDLdBContext: DbContext
     public DbSet<Zadania> Zadanias { get; set; }
     public DbSet<Zadania_zakonczone> Zadania_zakonczone { get; set; }
     public DbSet<Slownik> Slownik { get; set; }
+    public DbSet<Uzytkownik> Uzytkownik { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
