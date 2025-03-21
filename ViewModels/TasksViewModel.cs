37e5414982f0a749cd8860f7bfa086b9bcfefb25
Karolinova <karolina.nowinska13@gmail.com>
2025-03-21 20:45:26 +0100

    add TaskViewModel

---
+++ ViewModels/TasksViewModel.cs
@@ -0,0 +1,7 @@
+ internal class TasksViewModel
+ {
+    public List<Zadania>? Zadanias { get; set; }
+    public List<Zadania_zakonczone>? Zadania_Zakonczone { get; set; }
+    public List<SelectListItem>? Slownik { get; set; }
+    public Zadania Zadania { get; set; }
+ }
