15727f99e2f78da9c020b7b7f446fa71e27b61c1
Karolinova <karolina.nowinska13@gmail.com>
2025-04-18 19:46:05 +0200

    change internal to public

--- ViewModels/TasksViewModel.cs
+++ ViewModels/TasksViewModel.cs
@@ -1,4 +1,7 @@
- internal class TasksViewModel
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ToDoList_gh.Models;
+
+ public class TasksViewModel
  {
     public List<Zadania>? Zadanias { get; set; }
     public List<Zadania_zakonczone>? Zadania_Zakonczone { get; set; }
