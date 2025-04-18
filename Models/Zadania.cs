6c7cab7569bfe1305bf61a3f6a07b036e1c36ac4
Karolinova <karolina.nowinska13@gmail.com>
2025-04-18 20:03:25 +0200

    add validation to fields

--- Models/Zadania.cs
+++ Models/Zadania.cs
@@ -1,16 +1,25 @@
-[Table("Zadania")]
-public class Zadania
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList_gh.Models
 {
-    [Key]
-    public int Zadanie_id { get; set; }
-    public string Zadanie { get; set; } = string.Empty;
-    public string Opis { get; set; } = string.Empty;
-    public DateTime? DoTime { get; set; }
-    public DateTime StartTime { get; set; } = DateTime.Now;
-    
-    public int slownik_id { get; set; }
-    [ForeignKey("slownik_id")]
-    public Slownik? Slownik { get; set; }
-    
-    public Zadania_zakonczone? ZadanieZakonczone { get; set; }
+    [Table("Zadania")]
+    public class Zadania
+    {
+        [Key]
+        public int Zadanie_id { get; set; }
+        [Required(ErrorMessage = "Nazwa zadania jest wymagana")]
+        public string Zadanie { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Opis zadania jest wymagany")]
+        public string Opis { get; set; } = string.Empty;
+        public DateTime? DoTime { get; set; }
+        public DateTime StartTime { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Wybierz priorytet")]
+        
+        public int slownik_id { get; set; }
+        [ForeignKey("slownik_id")]
+        public Slownik? Slownik { get; set; }
+        
+        public Zadania_zakonczone? ZadanieZakonczone { get; set; }
+    }
 }
